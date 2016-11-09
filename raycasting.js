292c1a7b3fca0a6ec2949b44fe20f63733ee1f2a
rcornacchia <rlc2160@columbia.edu>
2016-11-08 22:53:45 -0500

    added html with canvas element and player, map objects in js

--- raycasting.js
+++ raycasting.js
@@ -1,1 +1,10 @@
-function Player
+function Player(x, y, direction) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+}
+
+function Map(size) {
+    this.size = size;
+    this.wallGrid = new Uint8Array(size * size);
+}
